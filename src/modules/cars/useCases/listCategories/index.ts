0c86f8ccbede0329343153fd8a54f1c79c0c03a5
tmrafinha4@gmail.com <moa2jvl@bosch.com>
2023-02-08 11:04:45 -0300

    project started

--- src/modules/cars/useCases/listCategories/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-import { ListCategoryController } from "./ListCategoriesController";
-import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
-
-const categoriesRepository = CategoriesRepository.getInstance();
-
-const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository)
-
-export const listCategoriesController = new ListCategoryController(listCategoriesUseCase);
---
+++ src/services/BookingService.ts
@@ -0,0 +1,4 @@
+export class BookingService {
+    constructor(){}
+    
+}
